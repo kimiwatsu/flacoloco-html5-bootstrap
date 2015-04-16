85d5485b7e3851d56d22c20edfb93165e0ec6e71
Alejandro Vásquez <alejandro.vasquez@doubleyou.com>
2015-04-16 16:01:22 +0200

    improved read.me file images added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,159 +1,160 @@
 module.exports = function( grunt ) {
 
-  // Configuration goes here 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+	// Configuration goes here 
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
 
 
-    // Stylus to css compilation
-    stylus: {
-        compile: {
-            files: {
-      		  'www/css/styles.css': 'dev/styl/*.styl' // compile and concat into single file
-    	     }
-        }
-    },    
+		// Stylus to css compilation
+		stylus: {
+				compile: {
+						files: {
+						'www/css/styles.css': 'dev/styl/*.styl' // compile and concat into single file
+					 }
+				}
+		},    
 
 
-    // Jade to html compilation
-    jade: {
-      compile: {
-        files: [ 
-          {
-            expand:true,
-            cwd: 'dev/jade',
-            src: '**/*.jade',
-            dest: 'www',
-            ext: '.html'
-          },
-          {
-            expand:true,
-            cwd: 'dev',
-            src: '*.jade',
-            dest: 'www',
-            ext: '.html'
-          },
-        ]
-      }
-    }, 
+		// Jade to html compilation
+		jade: {
+			compile: {
+				files: [ 
+					{
+						expand:true,
+						cwd: 'dev/jade',
+						src: '**/*.jade',
+						dest: 'www',
+						ext: '.html'
+					},
+					{
+						expand:true,
+						cwd: 'dev',
+						src: '*.jade',
+						dest: 'www',
+						ext: '.html'
+					},
+				]
+			}
+		}, 
 
 
-    // Move other files to production folder
-    copy: {
-      statics: {
-        files: [
-          {
-            expand: true,
-            cwd: 'dev/static/img',
-            src: '**',
-            dest: 'www/static/img',
-            filter: 'isFile'
-          },
-          {
-            expand: true,
-            cwd: 'dev/styl/img',
-            src: '**',
-            dest: 'www/css/img',
-            filter: 'isFile'
-          },          
-          {
-            expand: true,
-            cwd: 'dev/css',
-            src: '**/*.*',
-            dest: 'www/css',
-            filter: 'isFile'
-          }         
+		// Move other files to production folder
+		copy: {
+			statics: {
+				files: [
+					{
+						expand: true,
+						cwd: 'dev/static/img',
+						src: '**',
+						dest: 'www/static/img',
+						filter: 'isFile'
+					},
+					{
+						expand: true,
+						cwd: 'dev/styl/img',
+						src: '**',
+						dest: 'www/css/img',
+						filter: 'isFile'
+					},          
+					{
+						expand: true,
+						cwd: 'dev/css',
+						src: '**/*.*',
+						dest: 'www/css',
+						filter: 'isFile'
+					}         
 
-        ],
-      },
-      scripts: {
-        files: [
-          {
-            expand: true,
-            cwd: 'dev/js',
-            src: '**/*.js',
-            dest: 'www/js',
-            filter: 'isFile',
-          }
+				],
+			},
+			scripts: {
+				files: [
+					{
+						expand: true,
+						cwd: 'dev/js',
+						src: '**/*.js',
+						dest: 'www/js',
+						filter: 'isFile',
+					}
 
-        ]       
-      },
-      data: {
-        files: [
-          {
-            expand: true,
-            cwd: 'dev/json',
-            src: '**/*.json',
-            dest: 'www/json',
-            filter: 'isFile',
-          }
-        ]               
-      }
-     
-    },
+				]       
+			},
+			data: {
+				files: [
+					{
+						expand: true,
+						cwd: 'dev/json',
+						src: '**/*.json',
+						dest: 'www/json',
+						filter: 'isFile',
+					}
+				]               
+			}
+		 
+		},
 
-   //tinyPNG
-    tinypng: {
-      options: {
-          apiKey: "CsGs0xFYkboJ74ln-nVY9G14TLeUXQjO",
-          checkSigs: true,
-          sigFile: 'dev/file_sigs.json',
-          summarize: true,
-          showProgress: true,
-          stopOnImageError: true
-      },
-      main: {
-          expand:true,
-          cwd: 'dev/static/img',
-          src: '**/*', 
-          dest: 'www/static/img'
-      }
-    },
+	 //tinyPNG
+		tinypng: {
+			options: {
+					apiKey: "CsGs0xFYkboJ74ln-nVY9G14TLeUXQjO",
+					checkSigs: true,
+					sigFile: 'dev/file_sigs.json',
+					summarize: true,
+					showProgress: true,
+					stopOnImageError: true
+			},
+			main: {
+					expand:true,
+					cwd: 'dev/static/img',
+					src: '**/*', 
+					dest: 'www/static/img'
+			}
+		},
 
-    connect: {
-      server: {
-        options: {
-          hostname: 'localhost',
-          port: 2211,
-          base: 'www'
-        }
-      }
-    },
+		connect: {
+			server: {
+				options: {
+					hostname: 'localhost',
+					port: 2211,
+					base: 'www'
+				}
+			}
+		},
 
 
 
-  	 watch: {
-        stylus:{
-          files: ['dev/styl/*.styl'],
-          tasks: ['stylus']
-        },
-        jade:{
-          files: ['dev/jade/*.jade'],
-          tasks: ['jade']
-        },
-        js:{
-          files: ['dev/js/*.js','dev/lib/**/*.js'],
-          tasks: ['copy:scripts']
-        },
-        json:{
-          files: ['dev/json/*.json'],
-          tasks: ['copy:data']
-        }
-    }
+		 watch: {
+				stylus:{
+					files: ['dev/styl/*.styl'],
+					tasks: ['stylus']
+				},
+				jade:{
+					files: ['dev/jade/*.jade'],
+					tasks: ['jade']
+				},
+				js:{
+					files: ['dev/js/*.js','dev/lib/**/*.js'],
+					tasks: ['copy:scripts']
+				},
+				statics:{
+					files: ['dev/static/*.*'],
+					tasks: ['copy:statics']
+				},
 
-  });
+		}
 
-  // Load plugins here
-  grunt.loadNpmTasks('grunt-contrib-jade');
-  grunt.loadNpmTasks('grunt-contrib-stylus');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-tinypng');
+	});
 
-  grunt.registerTask('png',  ['tinypng']);
-  grunt.registerTask('default',  ['jade','stylus','copy','connect','watch']);
+	// Load plugins here
+	grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-contrib-stylus');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-tinypng');
+
+	grunt.registerTask('png',  ['tinypng']);
+	grunt.registerTask('default',  ['jade','stylus','copy','connect','watch']);
 
 
 };
--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # flacoloco-html5-bootstrap
-My bootstrap to start projects with HTML5: Jade, Stylus, JS and Grunt
+####My bootstrap to start responsive HTML5 projects: 
 
+#####Technologies
+* [Jade](http://jade-lang.com): to markup
+* [Stylus](https://learnboost.github.io/stylus/): to styles
+* [JS](http://vanilla-js.com/): to interaction
+
+#####Dependencies:
+* [Node.js](http://nodejs.org)
+* [Grunt](http://gruntjs.com/getting-started).
+
+#####To start:
 1. sudo npm install
 2. grunt
 3. http://localhost:2211
